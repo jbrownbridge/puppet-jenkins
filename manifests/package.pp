a78c9b783f86fbfc71fb707373964bf309d03696
Jason Brownbridge <jbrownbridge@gmail.com>
2012-06-08 23:53:45 +0200

    Fixed edge case.
    If jenkins is already installed then package doesn't get called which means
    puppet can't find the jenkins user and group.

--- manifests/package.pp
+++ manifests/package.pp
@@ -3,17 +3,4 @@ class jenkins::package {
     'jenkins' :
       ensure => installed;
   }
-
-  # Stub for group created by package
-  group { 'jenkins': }
-  
-  # Add jenkins user shadow group for PAM authentication
-  user {
-    'jenkins' :
-      ensure => present,
-      groups => shadow;
-  }
-
-  Package['jenkins'] -> Group['jenkins'] -> User['jenkins']
 }
-
