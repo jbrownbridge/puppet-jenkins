53dd89d23cd4e200e24e40ce16df5b820d1ed9dc
Jason Brownbridge <jbrownbridge@gmail.com>
2012-06-09 01:29:42 +0200

    Fix for apache modules not being activated.

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,5 +1,14 @@
 class jenkins::vhost($vhost_name='*', $vhost_port=80) {
   include apache
+
+  a2mod { "Enable proxy mod":
+    name    => "proxy",
+    ensure  => "present",
+  } ->
+  a2mod { "Enable proxy_http mod":
+    name    => "proxy_http",
+    ensure  => "present",
+  } ->
   apache::vhost::proxy { 'jenkins':
     vhost_name  => $vhost_name,
     port        => $vhost_port,
